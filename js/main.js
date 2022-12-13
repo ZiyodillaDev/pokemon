608002134f5051dd0a4146671c270737e0b6ce39
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2022-12-13 20:46:19 +0500

    Changed by ZyodillaDEv

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 // Navbar Shrink
 window.onscroll = function () {
     scrollFunction();
-  };
-  
-  function scrollFunction() {
+};
+
+function scrollFunction() {
     if (document.documentElement.scrollTop > 150) {
-      document.getElementById("navbar").classList.add("navbar-shrink");
+        document.getElementById("navbar").classList.add("navbar-shrink");
     } else {
-      document.getElementById("navbar").classList.remove("navbar-shrink");
+        document.getElementById("navbar").classList.remove("navbar-shrink");
     }
-  }
+}
+
+//   backtop
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 300) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
 
 // Main codes  
 
@@ -17,23 +29,61 @@ let pokiList = document.querySelector(".pokemon-list");
 
 let renderPokemons = (pokemon) => {
     let res = "";
-     list = document.createElement("div");
-    list.setAttribute("class", "col-12 col-sm-6 col-lg-3");
-  for (i of pokemon) {
-      res += `
-        <div class="card" style="width: 20rem;">
-        <img src="${i.img}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${i.title}</h5>
-        <p class="card-text">${i.summary}</p>
-        <a href="" class="btn btn-primary">More...</a>
-        </div>
-        </div>
-        `;
-    list.innerHTML = res;
-    }
-pokiList.appendChild(list);
-
+    list = document.createElement("div");
+    list.setAttribute("class", "row gap-3 justify-content-center pokemon-list");
+    for (i of pokemon) {
+    res += `
+    <div class="card col-12 col-sm-6 col-md-4 col-lg-3 card-bg cardHover" style="width: 19rem;position:relative;">
+    <span class="fw-bold text-warning id" style="font-size:20px;">#${i.id}</span>
+    <img src="${i.img}" class="card-img-top m-auto img" alt="..." style="width:150px;height:120px;">
+    <div class="card-title text-center pokemonValue2 fw-bold pb-2 text-warning pokName" style="display:none; font-size:22px">${i.name}</div>
+    <div class="card-title text-center pokemonValue2 fw-bold pb-2 text-warning pokName" style="display:none; font-size:22px">Height:  ${i.height}</div>
+    <div class="card-title text-center pokemonValue2 fw-bold pb-2 text-warning pokName" style="display:none; font-size:22px">Weight:  ${i.weight}</div>
+    <button class="card-title  text-center pokemonValue2 fw-bold pb-2 pokName text-white text-bg-info" style="display:none; font-size:22px">Choose</button>
+    <div class="card-body">
+    <h5 class="card-title text-center pokemonValue2 fw-bold pb-2 text-warning" style="font-size:22px; z-index:99999;">${i.name}</h5>
+    <div class="d-flex flex-row-reverse gap-4 justify-content-between">
+    <div class="d-flex  flex-column">
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">Candies: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.candy_count}</p>
+    </div>
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">Egg: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.egg}</p>
+    </div>
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">Spawn: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.spawn_chance}</p>
+    </div>
+    </div>
+    <div class="d-flex flex-column">
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">Height: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.height}</p>
+    </div>
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">Weight: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.weight}</p>
+    </div>
+    <div class="d-flex gap-2">
+    <p class="card-text text-center pokemonValue3 fw-bold text-warning">avgs: </p>
+    <p class="card-text text-center pokemonValue text-white">${i.avg_spawns}</p>
+    </div>
+    </div>
+    </div>
+    <div class"d-flex align-items-center justify-content-center ">
+    <span class="card-text text-center pokemonValue3 fw-bold text-white py-2 px-4 text-bg-warning">${i.spawn_time}</span>
+    </div>
+    </div>
+    <div class="d-flex flex-column">
+    </div>
+    </div>
+    `;
+   }
+        list.innerHTML = res;
+        pokiList.appendChild(list);
+    
 };
 
 renderPokemons(pokemons);
